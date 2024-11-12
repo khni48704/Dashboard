d7e9930383859e3d65bc6aaa4c26f6acb1e3ecfe
SigneWehlast <119939548+SigneWehlast@users.noreply.github.com>
2024-11-12 10:25:00 +0100

    login - der ikke virker

---
+++ public/js/login.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const app = express();
+const db = require('../config/db.js');
+
+app.post ('/login', (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+     return res.status(500).send('Wrong email or password');
+    }
+
+        db.query(
+      'SELECT * FROM users WHERE email = ? AND password = ?',
+      [email, password],
+      (error, results) => {
+        if (error) {
+          return res.status(500).send('Wrong email or password');
+        }
+
+        if (results.length > 0) {
+          res.json({ message: 'Velkommen ${email}'});
+        } else {
+          return res.status(500).send('Wrong email or password');
+        }
+      }
+    )
+})
+
+document.getElementById('login_btn').addEventListener('click', () => {
+  const username = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  fetch('http://localhost:3200/login', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ email, password })
+  })
+  .then((res) => res.json())
+  .then((res) => {
+      console.log(res);
+      const messageElement = document.getElementById('message');
+      if (res.message === 'Velkommen') {
+          messageElement.textContent = res.message;
+          messageElement.style.color = 'green';
+      } else {
+          messageElement.textContent = res.message;
+          messageElement.style.color = 'red';
+      }
+  })
+  .catch((err) => {
+      console.log(err);
+      const messageElement = document.getElementById('message');
+      messageElement.textContent = 'Der opstod en fejl. Prøv igen.';
+      messageElement.style.color = 'red';
+  });
+});
