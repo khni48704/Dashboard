9a822f4437aa803a2ef5face409e36e507def4ea
SigneWehlast <119939548+SigneWehlast@users.noreply.github.com>
2024-11-27 11:44:36 +0100

    createstack kan tilgås

--- src/routes/web.js
+++ src/routes/web.js
@@ -29,12 +29,12 @@ router.get('/projects', (req, res) => {
 });
 
 router.get('/createStack', (req, res) => {
-    const emailSent = req.query.email;
+    /*const emailSent = req.query.email;
     console.log(emailSent);
     if (!emailSent) {
         return res.status(400).send('E-mail parameter mangler.');
-    }
-    res.render('createStack', { emailSent })
+    }*/
+    res.render('createStack'/*, { emailSent }*/)
 });
 
 router.get('/createTemplate', (req, res) => {
